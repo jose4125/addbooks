2d2d44f975cee126e831476b24016fd90363ef62
jose david lombana <jose4125@gmail.com>
2015-02-06 10:23:16 -0500

    teble dynamic from fields form

--- app/scripts/books/books.list.js
+++ app/scripts/books/books.list.js
@@ -1,7 +1,7 @@
 'use strict';
 (function () {
   angular.module('app.books').directive('booksList', booksList);
-  function booksList(storage) {
+  function booksList(storage, fields) {
     return {
       templateUrl: 'app/scripts/books/bookslist.view.html',
       replace: true,
@@ -9,10 +9,16 @@
         modelBooks: '='
       },
       link: function postLink(scope) {
+        scope.fields = fields.getFormFields();
+        console.log('fields', scope.fields);
         scope.removeBook = removeBook;
+        scope.edit = edit;
         function removeBook(index) {
           scope.modelBooks.splice(index, 1);
           storage.removeBooks(index);
+        }
+        function edit() {
+          console.log('edit');
         }
       }
     };
