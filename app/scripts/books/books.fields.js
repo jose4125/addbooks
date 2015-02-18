a50903666304fa2117b8163e703a586ffa65b437
jose david lombana <jose4125@gmail.com>
2015-02-18 10:41:15 -0500

    localstorage formfields in the run function

--- app/scripts/books/books.fields.js
+++ app/scripts/books/books.fields.js
@@ -4,65 +4,6 @@
 
   function fields() {
     var yearList = [];
-
-    var formFields = [{
-      name: 'bookName',
-      required: true,
-      messages: {
-        required: 'the Book name cant be blank'
-      },
-      params: {
-        view: 'input',
-        type: 'text'
-      }
-    },
-    {
-      name: 'author',
-      required: true,
-      messages: {
-        required: 'the Author name cant be blank'
-      },
-      params: {
-        view: 'input',
-        type: 'text'
-      }
-    },
-    {
-      name: 'year',
-      required: true,
-      messages: {
-        required: 'the Year cant be blank'
-      },
-      params: {
-        view: 'select',
-        options: {
-          start: 2005,
-          end: 2015
-        }
-      }
-    },
-    {
-      name: 'price',
-      required: true,
-      pattern: '^[0-9]+$',
-      messages: {
-        required: 'the Price cant be blank',
-        pattern: 'you should type only numbers'
-      },
-      params: {
-        view: 'input',
-        type: 'text'
-      }
-    },
-    {
-      name: 'newBook',
-      required: false,
-      params: {
-        view: 'input',
-        type: 'checkbox'
-      }
-    }
-    ];
     function getUserFiedls() {
       return JSON.parse(window.localStorage.getItem('formFields'));
     }
--- app/scripts/books/books.router.js
+++ app/scripts/books/books.router.js
@@ -1,6 +1,6 @@
 'use strict';
 (function () {
-  angular.module('app.books').config(booksRouter);
+  angular.module('app.books').config(booksRouter).run(runFunction);
 
   function booksRouter($stateProvider, $urlRouterProvider) {
     $stateProvider
@@ -16,5 +16,66 @@
       }
     });
     $urlRouterProvider.otherwise('/');
+  }
+  function runFunction() {
+    var formFields = [{
+      name: 'bookName',
+      required: true,
+      messages: {
+        required: 'the Book name cant be blank'
+      },
+      params: {
+        view: 'input',
+        type: 'text'
+      }
+    },
+    {
+      name: 'author',
+      required: true,
+      messages: {
+        required: 'the Author name cant be blank'
+      },
+      params: {
+        view: 'input',
+        type: 'text'
+      }
+    },
+    {
+      name: 'year',
+      required: true,
+      messages: {
+        required: 'the Year cant be blank'
+      },
+      params: {
+        view: 'select',
+        options: {
+          start: 2005,
+          end: 2015
+        }
+      }
+    },
+    {
+      name: 'price',
+      required: true,
+      pattern: '^[0-9]+$',
+      messages: {
+        required: 'the Price cant be blank',
+        pattern: 'you should type only numbers'
+      },
+      params: {
+        view: 'input',
+        type: 'text'
+      }
+    },
+    {
+      name: 'newBook',
+      required: false,
+      params: {
+        view: 'input',
+        type: 'checkbox'
+      }
+    }
+    ];
+    window.localStorage.setItem('formFields', JSON.stringify(formFields));
   }
 })();
